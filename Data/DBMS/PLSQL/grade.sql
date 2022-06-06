f2b493ce5ba95bec673ccac44dd202645526b2c8
Soumyadeep Pal <64325089+soumyadeep2002ss@users.noreply.github.com>
2022-06-07 01:18:04 +0530

    change kore diyechi dekhis

--- Data/DBMS/PLSQL/grade.sql
+++ Data/DBMS/PLSQL/grade.sql
@@ -6,7 +6,7 @@ begin
     if m>=90 and m<=100 then g:='0';
     elsif m>=80 and m<=89 then g:='E';
     elsif m>=70 and m<=79 then g:='A';
-    elsif m>60 and m<=69 then g:='B';
+    elsif m>=60 and m<=69 then g:='B';
     else g:='F';
     end if;
     dbms_output.put_line('Grade is '|| g);
